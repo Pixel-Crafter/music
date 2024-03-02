ab051369dd66a8c80a00c1de0072d4430fc7bc85
Harshal <88491916+Pixel-Crafter@users.noreply.github.com>
2024-03-02 20:40:56 +0530

    new files

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,11 @@
 
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -14,11 +19,13 @@ const Sidebar: React.FC<SidebarProps> = ({
 
     const routes = useMemo(() => [
         {
+            icon: HiHome,
             label: 'Home',
             active: pathname !== '/search',
             href: '/',
         },
         {
+            icon: BiSearch,
             label: 'Search',
             active: pathname === '/search',
             href: '/search',
@@ -26,8 +33,38 @@ const Sidebar: React.FC<SidebarProps> = ({
     ], [pathname]);
 
     return (
-        <div>
-            {children}
+        <div className="flex h-full">
+            <div className="
+                hidden
+                md:flex
+                flex-col
+                gap-y-2
+                bg-black
+                h-full
+                w-[300px]
+                p-2
+                "> 
+                <Box className="
+                    flex
+                    flex-col
+                    gap-y-4
+                    px-5
+                    py-4
+                ">
+                    {routes.map((item) => (
+                        <SidebarItem
+                            key={item.label}
+                            {...item}
+                        />
+                    ))}
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    Song Library
+                </Box>
+            </div>
+            <main>
+                {children}
+            </main>
         </div>
     );
 }
