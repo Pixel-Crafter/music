9edd99d43e2541dfbfbf59ddc6e8c487f14a5a9c
Harshal <88491916+Pixel-Crafter@users.noreply.github.com>
2024-03-02 13:33:12 +0530

    layout

---
+++ components/Sidebar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+
+interface SidebarProps {
+    children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+    children
+}) => {
+    const pathname = usePathname();
+
+    const routes = useMemo(() => [
+        {
+            label: 'Home',
+            active: pathname !== '/search',
+            href: '/',
+        },
+        {
+            label: 'Search',
+            active: pathname === '/search',
+            href: '/search',
+        }
+    ], [pathname]);
+
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
+
+export default Sidebar;
