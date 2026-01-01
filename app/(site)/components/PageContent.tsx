cb23fbe17185b2ebccf780cff8bbd050789fcb52
Harshal <88491916+Pixel-Crafter@users.noreply.github.com>
2026-01-01 15:04:20 +0530

    Media Player Function

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { Song } from "@/types";
 import SongItem from "../../../components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
+import { Song } from "@/types";
 
 interface PageContentProps {
     songs: Song[];
 }
 
 const PageContent: React.FC<PageContentProps> = ({ songs }) => {
+    const onPlay = useOnPlay(songs);
 
     if (songs.length === 0) {
         return (
@@ -33,7 +35,7 @@ const PageContent: React.FC<PageContentProps> = ({ songs }) => {
             {songs.map((item) => (
                 <SongItem 
                     key={item.id}
-                    onClick={() => {}}
+                    onClick={(id: string) => onPlay(id)}
                     data={item}
                 />
             ))}
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,11 +4,14 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import { twMerge } from "tailwind-merge";
+
+import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
 
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
-import { Song } from "@/types";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -20,6 +23,7 @@ const Sidebar: React.FC<SidebarProps> = ({
     songs
 }) => {
     const pathname = usePathname();
+    const player = usePlayer();
 
     const routes = useMemo(() => [
         {
@@ -37,7 +41,12 @@ const Sidebar: React.FC<SidebarProps> = ({
     ], [pathname]);
 
     return (
-        <div className="flex h-full">
+        <div className={twMerge(`
+            flex
+            h-full
+        `,
+            player.activeId && 'h-[calc(100%-80px)]'
+        )}>
             <div className="
                 hidden
                 md:flex
