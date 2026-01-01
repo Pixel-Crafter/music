db28e25033d0fe6a0f642a2a27a155cf23b59c37
Harshal <88491916+Pixel-Crafter@users.noreply.github.com>
2026-01-01 15:15:16 +0530

    Playable Songs

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,6 +7,7 @@ import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
+import useOnPlay from "@/hooks/useOnPlay";
 
 import MediaItem from "./MediaItem";
 
@@ -20,6 +21,8 @@ const Library: React.FC<LibraryProps> = ({
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser();
+
+    const onPlay = useOnPlay(songs);
 
     const onClick = () => {
         if (!user) {
@@ -78,7 +81,7 @@ const Library: React.FC<LibraryProps> = ({
             ">
                 {songs.map((item) => (
                     <MediaItem
-                        onClick={() => {}}
+                        onClick={(id: string) => onPlay(id)}
                         key={item.id}
                         data={item}
                     />            
